e2fb6843eb820d1f2a1a2a5b7cb0f80f60ab9339
roberto <robertoramirezmoreno@gmail.com>
2023-10-04 16:26:47 +0100

    almostç

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
 
 
 
-    <div class="container">
+    <div class="container" style="margin-bottom: 70px;">
         <h1 style="margin-bottom: 30px; margin-top: 20px; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">Mis Proyectos</h1>
 
         <div class="row">
@@ -51,7 +51,7 @@
                         <h5 class="card-title" style="background-color:rgba(255, 255, 255, 0.8);">Agregador cervecerías y cervezas</h5>
             
                         <div class="scroll-box">
-                            <p style="color: rgb(7, 4, 4); text-align: justify;" class="card-text">
+                            <p style="color: rgb(249, 244, 244); text-align: justify;" class="card-text">
                               En este proyecto individual realicé un catálogo/agregador de cervecerías y cervezas. Para poder crear nuevas entradas es necesario estar registrado, además he añadido un mapa interactivo con Leaflet que marca la ubicación del bar en el mapa en función de las coordenadas añadidas desde el modo crear entrada. En la página HOME, para darle un toque entretenido, añadí un contador de cervezas que va sumando cada vez que pulsamos en una más y va modificando el mensaje según el número de cervezas.
                             </p>
                         </div>
@@ -61,22 +61,30 @@
             </div>
             
             <div class="col-md-4">
-                <div class="card">
-                    <img src="proyecto2.jpg" class="card-img-top" alt="Proyecto 2">
-                    <div class="card-body">
-                        <h5 class="card-title">Proyecto 2</h5>
-                        <p class="card-text">Descripción breve del Proyecto 2.</p>
-                        <a href="enlace_proyecto2.html" class="btn btn-primary">Ver Proyecto</a>
+                <div class="card" style="background-image: url('{{ asset('images/proyecto2.jpg') }}'); background-size: cover;">
+                    <div class="card-body" style="background-color: rgba(255, 255, 255, 0.2); /* Añade opacidad al fondo */">
+                        <h5 class="card-title" style="background-color:rgba(255, 255, 255, 0.8);">Tienda venta artículos de segunda mano</h5>
+            
+                        <div class="scroll-box">
+                            <p style="color: rgb(250, 250, 250); text-align: justify;" class="card-text">
+                              En este proyecto individual realicé un catálogo/agregador de cervecerías y cervezas. Para poder crear nuevas entradas es necesario estar registrado, además he añadido un mapa interactivo con Leaflet que marca la ubicación del bar en el mapa en función de las coordenadas añadidas desde el modo crear entrada. En la página HOME, para darle un toque entretenido, añadí un contador de cervezas que va sumando cada vez que pulsamos en una más y va modificando el mensaje según el número de cervezas.
+                            </p>
+                        </div>
+                        <a href="http://209.38.216.56/home" class="btn btn-primary mt-3" style="display: flex; justify-content: center;">Ver Proyecto</a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card">
-                    <img src="proyecto3.jpg" class="card-img-top" alt="Proyecto 3">
-                    <div class="card-body">
-                        <h5 class="card-title">Proyecto 3</h5>
-                        <p class="card-text">Descripción breve del Proyecto 3.</p>
-                        <a href="enlace_proyecto3.html" class="btn btn-primary">Ver Proyecto</a>
+                <div class="card" style="background-image: url('{{ asset('images/proyecto3.jpg') }}'); background-size: cover;">
+                    <div class="card-body" style="background-color: rgba(255, 255, 255, 0.2); /* Añade opacidad al fondo */">
+                        <h5 class="card-title" style="background-color:rgba(255, 255, 255, 0.8);">Medio de comunicación digital</h5>
+            
+                        <div class="scroll-box">
+                            <p style="color: rgb(255, 255, 255); text-align: justify;" class="card-text">
+                              En este proyecto individual realicé un catálogo/agregador de cervecerías y cervezas. Para poder crear nuevas entradas es necesario estar registrado, además he añadido un mapa interactivo con Leaflet que marca la ubicación del bar en el mapa en función de las coordenadas añadidas desde el modo crear entrada. En la página HOME, para darle un toque entretenido, añadí un contador de cervezas que va sumando cada vez que pulsamos en una más y va modificando el mensaje según el número de cervezas.
+                            </p>
+                        </div>
+                        <a href="http://209.38.216.56/home" class="btn btn-primary mt-3" style="display: flex; justify-content: center;">Ver Proyecto</a>
                     </div>
                 </div>
             </div>
