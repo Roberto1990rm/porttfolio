c33234071ae4206a9a8ed8fbd026045775d51ec9
roberto <robertoramirezmoreno@gmail.com>
2023-10-04 21:33:36 +0100

    fontawesome

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,9 +4,9 @@
             
             <div class="col-md-6">
                 <ul class="list-unstyled list-inline social-icons">
-                    <li class="list-inline-item"><a href="https://linkedin.com/tunombre" target="_blank"><i class="fab fa-linkedin" style="font-size: 1.5em;"></i></a></li>
-                    <li class="list-inline-item"><a href="https://github.com/tunombre" target="_blank"><i class="fab fa-github" style="font-size: 1.5em;"></i></a></li>
-                    <li class="list-inline-item"><a href="mailto:tucorreo@gmail.com"><i class="fas fa-envelope" style="font-size: 1.5em;"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.linkedin.com/in/roberto-ram%C3%ADrez-2272b1252/" target="_blank"><i class="fab fa-linkedin" style="font-size: 1.5em;"></i></a></li>
+                    <li class="list-inline-item"><a href="https://github.com/roberto1990rm" target="_blank"><i class="fab fa-github" style="font-size: 1.5em;"></i></a></li>
+                    <li class="list-inline-item"><a href="mailto:fullstackrober@gmail.com"><i class="fas fa-envelope" style="font-size: 1.5em;"></i></a></li>
                 </ul>
             </div>
             <div class="col-md-6">
