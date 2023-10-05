1d27d6305c8e5529381a218c3d466b575d8b13bb
roberto <robertoramirezmoreno@gmail.com>
2023-10-05 20:03:08 +0100

    aa

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>{{ $title ?? 'RobertoRamírez' }}</title>
+    <title>{{ $title ?? 'Roberfullstack.com' }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
@@ -15,7 +15,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
-    <title>{{ $title ?? 'fullstackrober.com' }}</title>
 </head>
 <body style="background-color: #e3fae1;">
     <x-navbar />
