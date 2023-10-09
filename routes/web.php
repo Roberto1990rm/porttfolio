d4374158a644ac3ea047e63137540ff99ae1dd17
roberto <robertoramirezmoreno@gmail.com>
2023-10-09 14:04:47 +0100

    translated

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController; // Agrega esta línea
 use App\Http\Controllers\publicController;
+use App\Http\Controllers;
 use App\Http\Middleware\App;
 
 /*
