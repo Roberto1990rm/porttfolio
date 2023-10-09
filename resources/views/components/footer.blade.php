5e8042388f2ced85863a425e289d7d498de45050
roberto <robertoramirezmoreno@gmail.com>
2023-10-09 18:02:33 +0100

    translated1

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,8 @@
 <footer>
-    <div class="container">
-        <div class="row mt-2">
+    <div class="container mb-1">
+        <div class="row mt-1" style="margin-bottom: -18px;">
             
-            <div class="col-md-6">
+            <div class="col-md-6" style="margin-bottom: -16px;">
                 <ul class="list-unstyled list-inline social-icons">
                     <li class="list-inline-item"><a href="https://www.linkedin.com/in/roberto-ram%C3%ADrez-2272b1252/" target="_blank"><i class="fab fa-linkedin" style="font-size: 1.5em;"></i></a></li>
                     <li class="list-inline-item"><a href="https://github.com/roberto1990rm" target="_blank"><i class="fab fa-github" style="font-size: 1.5em;"></i></a></li>
