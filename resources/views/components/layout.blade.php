7af1c351a08bda2e24fc33521b26dd94ce65f44e
roberto <robertoramirezmoreno@gmail.com>
2023-11-27 02:35:06 +0000

    mail changed

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,7 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Portfolico cv currículum Roberto Ramírez Moreno, programdor desarrollador web php, html, css, react, js">
+    <meta name="description" content="Portfolio, cv, currículum de Roberto Ramírez Moreno, programdor o desarrollador web php, html, css, react, js">
 
 
 </head>
